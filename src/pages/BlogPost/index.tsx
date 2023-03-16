649b0402a0e1519cccd67a50a8c959105bcd5754
vanor-cardozo <vanor.fc@gmail.com>
2023-03-16 17:58:25 -0300

    feat: Adiciona a página de BlogPost ao react router dom

---
+++ src/pages/BlogPost/index.tsx
@@ -0,0 +1,10 @@
+import { PostSummary } from './components/PostSummary'
+import { BlogPostContainer } from './styles'
+
+export function BlogPost() {
+  return (
+    <BlogPostContainer>
+      <PostSummary />
+    </BlogPostContainer>
+  )
+}
