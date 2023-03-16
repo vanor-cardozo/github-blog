1de88a4678790eece6a719ad052d507be9124bc1
vanor-cardozo <vanor.fc@gmail.com>
2023-03-16 16:48:21 -0300

    feat: Adiciona lógica para reazlizar o filtro na API

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,13 +1,30 @@
-import { SearchContainer } from './styles'
+import { useContext, useState } from 'react'
+import { BlogPostsContext } from '../../context/BlogPostsContext'
+import { InputAndButtonDiv, SearchContainer } from './styles'
 
 export function Search() {
+  const { getData } = useContext(BlogPostsContext)
+  const [searchInput, setSearchInput] = useState('')
+
+  function handleSearchInput(e: { target: { value: any } }) {
+    const stringToQuery = `${e.target.value.trim().replaceAll(' ', '+')}+`
+    setSearchInput(stringToQuery)
+  }
+
   return (
     <SearchContainer>
       <span>
         <h2>Publicações</h2>
         <p>6 publicações</p>
       </span>
-      <input type="text" placeholder="Buscar conteúdo" />
+      <InputAndButtonDiv>
+        <input
+          type="text"
+          placeholder="Insira uma palavra para pesquisar no blog"
+          onChange={handleSearchInput}
+        />
+        <button onClick={() => getData(searchInput)}>Buscar</button>
+      </InputAndButtonDiv>
     </SearchContainer>
   )
 }
--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -10,6 +10,10 @@ export const SearchContainer = styled.div`
     align-items: baseline;
     justify-content: space-between;
   }
+`
+
+export const InputAndButtonDiv = styled.div`
+  width: 100%;
 
   input {
     border: 2px solid ${(props) => props.theme['base-border']};
@@ -22,6 +26,23 @@ export const SearchContainer = styled.div`
 
     &::placeholder {
       color: ${(props) => props.theme['base-label']};
+    }
+  }
+
+  button {
+    position: absolute;
+    transform: translate(-75px);
+    margin-top: 1.4rem;
+    border: none;
+    border-radius: 5px;
+    background: ${(props) => props.theme['base-border']};
+    padding: 0.6rem;
+    color: ${(props) => props.theme.white};
+    cursor: pointer;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${(props) => props.theme['base-post']};
     }
   }
 `
